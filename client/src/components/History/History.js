63c57df1fc03d17b36aa534a0c6cfe6ec09e2632
Adit Jindal <54547947+aditjindal27@users.noreply.github.com>
2022-01-09 06:42:48 -0500

    Update History.js

--- client/src/components/History/History.js
+++ client/src/components/History/History.js
@@ -1,4 +1,4 @@
-import { Typography, Grid, Container } from '@material-ui/core'
+import { Typography, Grid, Container, CircularProgress } from '@material-ui/core'
 import React from 'react'
 import { useSelector } from 'react-redux';
 import useStyles from './styles';
@@ -7,23 +7,24 @@ import useStyles from './styles';
 const History = () => {
     
     const classes = useStyles();
-    const data = useSelector((state) => state);
+    const data = useSelector((state) => state.updateVal);
 
     return (
     <div>
         <div style={{justifyContent:'center', display:'flex'}}>
             <Typography variant="h5" className={classes.title}>HISTORY</Typography>
         </div>
+        {Object.keys(data).length>0?
         <Grid container spacing={9} >
             <Grid item>
                 <Grid container spacing={2} className={classes.paper}>  
                     <Grid item>
-                        {data.updateVal.expenses.map((expense, index) => (
+                        {data.expenses.map((expense, index) => (
                             <Typography  style={{color:'white'}}>{index+1}. ${expense} : </Typography>
                         ))}
                     </Grid>
                     <Grid item>
-                        {data.updateVal.expense_d.map((expense_d) => (
+                        {data.expense_d.map((expense_d) => (
                             <Typography style={{color:'white'}}>{expense_d}</Typography>
                         ))}
                     </Grid>
@@ -33,18 +34,20 @@ const History = () => {
             <Grid item>
                 <Grid container spacing={2} className={classes.paper}>  
                     <Grid item>
-                        {data.updateVal.incomes.map((income, index) => (
+                        {data.incomes.map((income, index) => (
                             <Typography  style={{color:'white'}}>{index+1}. ${income} : </Typography>
                         ))}
                     </Grid>
                     <Grid item>
-                        {data.updateVal.income_d.map((income_d) => (
+                        {data.income_d.map((income_d) => (
                             <Typography style={{color:'white'}}>{income_d}</Typography>
                         ))}
                     </Grid>
                 </Grid>
             </Grid>
         </Grid>
+        :
+        <CircularProgress/>}
     </div>
     )
 }
